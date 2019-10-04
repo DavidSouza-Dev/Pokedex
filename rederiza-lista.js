36eeffab4c8afde9759bc1e17ce063e58af56cb0
David <davidrs.souza@gmail.com>
2019-10-04 16:43:42 -0300

    alteracoes

--- rederiza-lista.js
+++ rederiza-lista.js
@@ -1,9 +1,8 @@
 renderizaLista();
-renderizaCard();
-teste();
+//renderizaCard();
 
 function renderizaLista(){
-    for (var i=1; i<=15; i++){
+    for (var i=1; i<=10; i++){
         
         $.ajax({
             url: `https://pokeapi.co/api/v2/pokemon/${i}`,
@@ -14,12 +13,14 @@ function renderizaLista(){
             },
             async: false,
             success: (resposta) => {
-                console.log(resposta);
+                //console.log(resposta);
+                
+                //variáveis
                 var id = ("000"+ resposta.id).slice(-3);
                 var nome = resposta.species.name.toUpperCase();
                 var avatar = resposta.sprites.front_default;
                 $(".linha").append(`
-                    <li id="${id}">
+                    <li id="${id}" value="${resposta.id}">
                         <span>ID: ${id}</span>
                         <a class="pokemon" href="index.html"> ${nome}    
                             <img class="minuatura" "height="40" width="40"src=${avatar}>
@@ -102,3 +103,98 @@ function renderizaLista(){
 
 console.log(teste); */
 
+
+$(document).ready(function(){
+    $(" li").click(function(){
+        $(".card").html("")
+        //$(".card").trigger('reset');
+        console.log(`${$(this).attr('value')}`);
+        var URL = `https://pokeapi.co/api/v2/pokemon/${$(this).attr('value')}`;
+        console.log(URL)
+        
+        $.ajax({
+            url:URL,
+            method: 'GET',
+            dataType:'json',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            success: (resposta) => {
+                console.log(resposta);
+                var nome = resposta.name.toUpperCase();
+                var imagem = resposta.sprites.front_default;
+                var altura = resposta.height;
+                var peso = resposta.weight;
+                
+                
+                $(".card").append(`
+                <h4>${nome}</h4>`).append(`<img class="avatar-card" "height="116" width="116"src=${imagem}>`);
+
+                $(".card").append(`
+                        <div class="altura">
+                            Height:    ${altura}'
+                        </div> 
+                        <div class="peso">
+                            Weight:    ${peso} lb's
+                        </div>
+                        <div class="tipo"></div>
+                        <div class="maisinfo" value="${$(this).attr('value')}">More <i class="fas fa-angle-double-right"> </i></div>
+                    `);
+
+                console.log (resposta.types[0])
+                for (var i=0; i<resposta.types.length; i++){
+                    console.log (resposta.types[i].type.name.toUpperCase())
+                    $(".tipo").append(`
+                        
+                            <div class="nome-tipo">
+                            ${resposta.types[i].type.name.toUpperCase()}
+                            </div>
+                        
+                        `) 
+                }
+                
+            
+            
+            } 
+        });   //fim ajax
+    
+    
+    });//fim li
+});//fim listener
+
+
+$(document).ready(function(){
+
+    $('.maisinfo').click(function(){ 
+        console.log("fui clicado");
+        $(this).closest('.maisinfo').remove();
+    });
+
+    /* $(".avatar").click(function(){
+        console.log("fui clicado");
+        $(".maisinfo").html("")
+        
+        console.log("fui clicado");
+         var URL = `https://pokeapi.co/api/v2/pokemon/${$(this).attr('value')}`;
+        console.log(URL)
+        
+        $.ajax({
+            url:URL,
+            method: 'GET',
+            dataType:'json',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            success: (resposta) => {
+               console.log(resposta)
+                
+                
+                
+            
+            
+            } 
+        });    //fim ajax
+    
+    
+    });//fim li*/
+});    
